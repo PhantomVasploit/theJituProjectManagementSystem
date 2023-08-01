efd1001ab51c1fd9322755986958270115f05745
Paul-Sanga <paulsanganyamawi@gmail.com>
2023-08-01 11:30:56 +0300

    Database creation

---
+++ api/src/database/tables/userTableCreation.sql
@@ -0,0 +1,12 @@
+CREATE TABLE usersTable(
+    id INT IDENTITY(1, 1) PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    is_verified BIT DEFAULT 0,
+    is_admin BIT DEFAULT 0
+)
+
+
+INSERT INTO usersTable(first_name, last_name, email, is_verified, is_admin)
+VALUES('Phantom', 'Jinx', 'phantom@gmail.com', 1, 1);
