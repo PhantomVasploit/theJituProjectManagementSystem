f33aeb47c1efd14f67d0431516eb58887d118526
Paul-Sanga <paulsanganyamawi@gmail.com>
2023-08-02 15:11:45 +0300

    employees handlers

--- api/src/database/procedures/updateUserProc.sql
+++ api/src/database/procedures/updateUserProc.sql
@@ -1,11 +1,5 @@
-USE ThejituProjectManagementDatabase;
-GO
-
-
-CREATE OR ALTER PROCEDURE updateUserProc(@id INT, @first_name VARCHAR(255), @last_name VARCHAR(255), @email VARCHAR(255))
+CREATE PROCEDURE updateUserAccountPROC(@id INT, @first_name VARCHAR(255), @last_name VARCHAR(255), @email VARCHAR(255))
 AS
 BEGIN
-    UPDATE usersTable
-    SET first_name = @first_name, last_name = @last_name, email = @email
-    WHERE id = @id
-END;
+    UPDATE user_table SET first_name = @first_name, last_name = @last_name, email = @email WHERE id = @id AND is_admin = 0
+END
