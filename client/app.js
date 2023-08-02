334eea3f08c3696f0b15ddc12e708ea77f48bde6
rachael-gatwiri <rachaelmuga1@gmail.com>
2023-08-02 16:31:46 +0300

    add changes

--- client/app.js
+++ client/app.js
@@ -1,9 +1,3 @@
 document.getElementById('signupBtn').addEventListener('click', function() {
-    window.location.href = 'signup.html';
-});
-document.getElementsByClassName('registerHere').addEventListener('click', function() {
-    window.location.href = 'signup.html';
-});
-document.getElementsByClassName('loginhere').addEventListener('click', function() {
-    window.location.href = 'landing.html';
+    window.location.href = 'login.html';
 });
