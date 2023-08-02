bb904e1cfc5a6c10f84aa62e356fcb9c61ba82e1
rachael-gatwiri <rachaelmuga1@gmail.com>
2023-08-02 15:28:34 +0300

    add files

---
+++ client/app.js
@@ -0,0 +1,9 @@
+document.getElementById('signupBtn').addEventListener('click', function() {
+    window.location.href = 'signup.html';
+});
+document.getElementsByClassName('registerHere').addEventListener('click', function() {
+    window.location.href = 'signup.html';
+});
+document.getElementsByClassName('loginhere').addEventListener('click', function() {
+    window.location.href = 'landing.html';
+});
