49eacf6fb3f15803ff97ddf098fc1cf6c184f9b0
Paul-Sanga <paulsanganyamawi@gmail.com>
2023-08-02 16:33:59 +0300

    basic api documentation

--- api/src/app.js
+++ api/src/app.js
@@ -3,12 +3,14 @@ const helmet = require('helmet')
 const morgan = require('morgan')
 const express = require('express')
 const bodyParser = require('body-parser')
+const swaggerUI = require('swagger-ui-express')
 require('dotenv').config()
 
 
 const app = express()
 const port = process.env.PORT
 const routes = require('./routes/routes')
+const swaggerDoc = require('./config/swagger.config.json')
 const { errorLogger } = require('./middleware/errorLogger')
 
 // setting upt the middlewares
@@ -19,6 +21,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.use('/api/v1/', routes)
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc))
 app.use(errorLogger)
 
 
