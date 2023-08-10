ca52bf881355745e34537282cb1d5ed9544c5cd5
rachael-gatwiri <rachaelmuga1@gmail.com>
2023-08-10 11:41:30 +0300

    test1

--- api/src/controller/auth.controller.js
+++ api/src/controller/auth.controller.js
@@ -1,104 +1,83 @@
-const mssql = require('mssql')
-const bcrypt = require('bcrypt')
+const mssql = require('mssql');
+const bcrypt = require('bcrypt');
 
-const { registrationSchema, loginSchema } = require('../utils/validator')
-const { sqlConfig } = require('../config/database.connection.config')
-const { hashPassword } = require('../utils/hash')
-const { createToken } = require('../utils/token.gen')
+const { registrationSchema, loginSchema } = require('../utils/validator');
+const { sqlConfig } = require('../config/database.connection.config');
+const { hashPassword } = require('../utils/hash');
+const { createToken } = require('../utils/token.gen');
 
-module.exports.employeeRegister=(req, res)=>{
-    const { firstName, lastName, email, password } = req.body
-    const { error } = registrationSchema.validate({ firstName, lastName, email, password })
-    if(error){
-        return res.status(400).json({error: error.message})
-    }else{
-       // check if email is registred
-       mssql.connect(sqlConfig)
-       .then((pool)=>{
-            pool.request()
-            .input('email', email)
-            .execute('fetchUserByEmailPROC')
-            .then((result)=>{
-                if(result.recordset.length > 0){
-                    return res.status(400).json({error: 'Account creation failed! This email is already registered'})
-                }else{
-                    hashPassword(password)
-                    .then((hashedPwd)=>{
-                        pool.request()
-                        .input('first_name', firstName)
-                        .input('last_name', lastName)
-                        .input('email', email)
-                        .input('password', hashedPwd)
-                        .execute('createNewUserPROC')
-                        .then((result)=>{
-                            const token = createToken({email, is_admin: 0})
-                            return res.status(201).json({message: 'Account created successfully', token, user: {firstName, lastName, email, is_admin: 0}})
-                        })
-                        .catch((e)=>{
-                            console.log(e.message);
-                            return res.status(500).json({error: `Internal server error, ${e.message}`})
-                        })
-                    })
-                    .catch((e)=>{
-                        console.log(e.message);
-                        return res.status(500).json({error: `Internal server error, ${e.message}`})
-                    })
-                }
-            })
-            .catch((e)=>{
-                console.log(e.message);
-                return res.status(500).json({error: `Internal server error, ${e.message}`})
-            })
-       })
-       .catch((e)=>{
-        console.log(e.message);
-        return res.status(500).json({error: `Internal server error, ${e}`})
-       })
-    }   
-}
+module.exports.employeeRegister = async (req, res) => {
+    const { firstName, lastName, email, password } = req.body;
+    const { error } = registrationSchema.validate({ firstName, lastName, email, password });
 
-
-module.exports.login = (req, res)=>{
-    const {email, password} = req.body
-    const {error} = loginSchema.validate({email, password})
-    if(error){
-        return res.status(422).json({error: error.message})
-    }else{
-        // check if email is registered
-        mssql.connect(sqlConfig)
-        .then((pool)=>{
-            pool.request()
-            .input('email', email)
-            .execute('fetchUserByEmailPROC')
-            .then((result)=>{
-                if(result.recordset.length <= 0){
-                    return res.status(400).json({error: 'This email is not registred'})
-                }else{
-                    bcrypt.compare(password, result.recordset[0].password)
-                    .then((valid)=>{
-                        if(valid){
-                            const token = createToken({email, is_admin: result.recordset[0].is_admin})
-                            const {password, is_verified, is_assigned, ...user} = result.recordset[0]
-                            return res.status(200).json({message: 'Login successful', token, user})
-                        }else{
-                            return res.status(400).json({error: 'Invalid login credentials'})
-                        }
-                    })
-                    .catch((e)=>{
-                        return res.status(500).json({error: `Internal server error, ${e.message}`})        
-                    })
-                }
-            })
-            .catch((e)=>{
-                return res.status(500).json({error: `Internal server error, ${e.message}`})    
-            })
-        })
-        .catch((e)=>{
-            return res.status(500).json({error: `Internal server error, ${e.message}`})
-        })
+    if (error) {
+        return res.status(400).json({ error: error.message });
     }
-}
 
+    try {
+        const pool = await mssql.connect(sqlConfig);
+
+        const result = await pool.request()
+            .input('email', email)
+            .execute('fetchUserByEmailPROC');
+
+        if (result.recordset.length > 0) {
+            return res.status(400).json({ error: 'Account creation failed! This email is already registered' });
+        }
+
+        const hashedPwd = await hashPassword(password);
+
+        await pool.request()
+            .input('first_name', firstName)
+            .input('last_name', lastName)
+            .input('email', email)
+            .input('password', hashedPwd)
+            .execute('createNewUserPROC');
+
+        const token = createToken({ email, is_admin: 0 });
+        return res.status(201).json({
+            message: 'Account created successfully',
+            token,
+            user: { firstName, lastName, email, is_admin: 0 }
+        });
+    } catch (e) {
+        console.log(e.message);
+        return res.status(500).json({ error: `Internal server error, ${e.message}` });
+    }
+};
+
+module.exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    const { error } = loginSchema.validate({ email, password });
+
+    if (error) {
+        return res.status(422).json({ error: error.message });
+    }
+
+    try {
+        const pool = await mssql.connect(sqlConfig);
+
+        const result = await pool.request()
+            .input('email', email)
+            .execute('fetchUserByEmailPROC');
+
+        if (result.recordset.length <= 0) {
+            return res.status(400).json({ error: 'This email is not registered' });
+        }
+
+        const valid = await bcrypt.compare(password, result.recordset[0].password);
+
+        if (valid) {
+            const token = createToken({ email, is_admin: result.recordset[0].is_admin });
+            const { password, is_verified, is_assigned, ...user } = result.recordset[0];
+            return res.status(200).json({ message: 'Login successful', token, user });
+        } else {
+            return res.status(400).json({ error: 'Invalid login credentials' });
+        }
+    } catch (e) {
+        return res.status(500).json({ error: `Internal server error, ${e.message}` });
+    }
+};
 
 
 
--- api/src/database/procedures/getAllUserProc.sql
+++ api/src/database/procedures/getAllUserProc.sql
@@ -6,3 +6,5 @@ AS
 BEGIN
     SELECT * FROM usersTable WHERE is_admin = 0;
 END
+
+SELECT * FROM usersTable;
